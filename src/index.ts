72b259700898f2d3336f6e61e5b34b3ebb49487f
Basti Ortiz <ortiz@bastidood.dev>
2025-07-26 20:21:58 +0800

    feat: add plugin metadata

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,11 @@ import type { ESLint } from 'eslint';
 import { sortImports } from './rule.js';
 
 export default {
+  meta: {
+    name: 'eslint-plugin-imsort',
+    version: '0.1.0',
+    // @ts-expect-error - namespace is a valid property
+    namespace: 'imsort',
+  },
   rules: { 'imsort/sort-imports': sortImports },
 } satisfies ESLint.Plugin;
