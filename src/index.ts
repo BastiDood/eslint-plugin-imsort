b56eb5c5b9ed3ccb73d853d5811e5b26ac7b3f8b
Basti Ortiz <ortiz@bastidood.dev>
2025-08-08 03:09:26 +0800

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { sortImports } from './rule.ts';
 export default {
   meta: {
     name: '@bastidood/eslint-plugin-imsort',
-    version: '0.9.1',
+    version: '0.10.0',
     namespace: '@bastidood/imsort',
   },
   rules: { 'sort-imports': sortImports },
